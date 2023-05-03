2743b7f63469131d518a309d0a9298ca9563caaa
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-05-03 02:30:55 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -7,19 +7,19 @@
 function calculateThePriceOfTheDrink() {
   // We need to declare variables to store the price, size and tax of the selected drink
   let price
-  let size = document.getElementById("select-size").value
   let drink = document.getElementById("select-drink").value
-  const TAX = 0.13
+  const taxrate = 0.13
+alert(drink);
 
   // Calculate the price of the selected drink based on size and tax
   switch (drink) {
-    case "Strawberries & Creme Frappuccino Grande":
+    case "Strawberries and Creme Frappuccino Grande":
       price = 4.45
       break
-    case "Strawberries & Creme Frappuccino Venti":
+    case "Strawberries and Creme Frappuccino Venti":
       price = 4.95
       break
-    case "Pumpkin Spice Latte Grand":
+    case "Pumpkin Spice Latte Grande":
       price = 4.95
       break
     case "Caramel Macchiato Venti":
@@ -29,28 +29,15 @@ function calculateThePriceOfTheDrink() {
       price = 0
       break
   }
-
-  // Add the cost of the size to the drink and add the tax equal price
-  switch (size) {
-    case "Grande":
-      price += 1
-      break
-    case "Venti":
-      price += 1.5
-      break
-    default:
-      break
-  }
+  alert(price);
 
   // prossces
-  let totalPrice = 0
-  if (selectDink in price) {
-    const subTotal = price + price
-    taxPaid = subTotal * tax
-    total = subTotal + taxPaid
-  }
+tax = (price + price) * taxrate
+subtotal = price + price
+tax = subtotal * taxrate
+total = subtotal + tax
 
-  alert("totalPrice")
+alert("abc");
   // output
-  document.getElementById("answer").innerHTML
+  document.getElementById("answer").innerHTML = "abc"
 }
